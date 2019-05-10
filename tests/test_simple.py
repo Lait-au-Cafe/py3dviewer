b9de9ebbd2a793bedf4fbbf4dcb4ff423b89de98
Lait-au-Cafe <laitaucafe8@gmail.com>
2019-05-10 19:11:53 +0900

    Create test directory and move test script there.

---
+++ tests/test_simple.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import py3dviewer
+
+if __name__ == "__main__":
+    model_vertices = [
+         10,  10, 0., 
+        -10,  10, 0., 
+         10, -10, 0., 
+        -10, -10, 0.]
+
+    model_uvmap = [
+        1.0, 1.0, 
+        0.0, 1.0, 
+        1.0, 0.0, 
+        0.0, 0.0]
+
+    viewer = py3dviewer.Viewer(model_vertices, model_uvmap, "img/invader.png", "Sample")
+    while True:
+        if not viewer.update():
+            print("Exit. ")
+            break
--- tests/test_update_model.py
+++ tests/test_update_model.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import py3dviewer
 
 if __name__ == "__main__":
